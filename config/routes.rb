8e054f412e00335c7bbc2fa306f63e233577e1f7
Shinji <tanafamy_194@hotmail.com>
2021-01-21 14:31:50 +0900

    新規登録の画面上でエラーが出ている状態でリロードするとルーティングエラーが出る問題の解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  resources :users, only: [:new, :create]
   resources :posts, only: :index
+  # get 'users', :to => 'users#create'
 end
