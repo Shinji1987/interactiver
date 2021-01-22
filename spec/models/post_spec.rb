a04e7b0020b4dfb18f2523a268f125d0dfb2db56
Shinji <tanafamy_194@hotmail.com>
2021-01-22 20:23:03 +0900

    投稿機能のテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @post = FactoryBot.build(:post)
+      @post.post_image = fixture_file_upload('app/assets/images/Yokosuka.jpg')
+    end
+
+    describe 'テキスト・画像の投稿' do
+      context 'テキスト・画像の投稿ができる場合' do
+        it '必要な情報を適切に入力すると,テキスト・画像の投稿ができること' do
+          expect(@post).to be_valid
+        end
+        it '画像がなくても,投稿ができること' do
+          @post.post_image = nil
+          expect(@post).to be_valid
+        end
+      end
+
+      context '商品の出品ができない場合' do
+        it 'テキストを入力しないと投稿できない' do
+          @post.text = ""
+          @post.valid?
+          expect(@post.errors.full_messages).to include("テキストを入力してください")
+        end
+      end
+    end
+  end
 end
