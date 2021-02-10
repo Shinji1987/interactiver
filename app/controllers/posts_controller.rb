b8929e40290cafe3af61113d763c1d1e8c4a0719
Shinji <tanafamy_194@hotmail.com>
2021-02-10 22:55:49 +0900

    デザイン修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,12 +21,14 @@ class PostsController < ApplicationController
 
       @friends = FriendRequest.where("from_user_id = ? or to_user_id = ?", current_user.id, current_user.id).where(requesting_status: 2)
       friend_ids_dup = @friends.pluck(:from_user_id, :to_user_id).flatten!
+      @friend_stock = []
       if friend_ids_dup != nil
         @friend_ids = friend_ids_dup.uniq
         @friend_ids.delete(current_user.id)
       end
       # 自分の全ての友達のIDを配列で取得
     end
+
     @users_record = User.search(params[:keyword])
     @users = @users_record.select(:id)
 
