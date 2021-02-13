99e43dc04a6c89444beab94de4a11146b3e8a94b
Shinji <tanafamy_194@hotmail.com>
2021-02-13 15:25:48 +0900

    LoggerSilenceの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,26 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
   before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :mylogger_test
 
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:image, :nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday, :profile, :shop_address])
+  end
+
+
+  class MyLogger < Logger
+    include LoggerSilence
+    include ActiveSupport::LoggerThreadSafeLevel
+  end
+
+  private
+
+  def mylogger_test
+    mylogger = MyLogger.new(STDOUT)
+    mylogger.silence do
+      mylogger.debug("controller = #{controller_name}")
+      mylogger.info("action = #{action_name}")
+      mylogger.error("controler#action = #{controller_name}##{action_name}")
+    end
   end
 end
