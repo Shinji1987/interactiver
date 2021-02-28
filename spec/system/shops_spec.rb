90bcd95efed609148437e55875137b927d51db35
Shinji <tanafamy_194@hotmail.com>
2021-02-28 16:58:34 +0900

    詳細ページ、ユーザー編集、店舗情報編集、検索機能、友達機能、チャット機能の結合試験

---
+++ spec/system/shops_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe "店舗編集", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @shop = FactoryBot.build(:shop)
+  end
+
+  context '店舗情報の編集ができるとき' do 
+    it 'ログインしたら店舗情報を編集できる' do
+      # ログインする
+      sign_in(@user)
+      # 詳細ページに移動する
+      visit user_path(@user.id)
+      # 店舗情報編集画面へ移動する
+      find('input[name="shop_edit_btn"]').click
+      # 最初は店舗情報フォームが空になっていることを確認する
+      expect(
+        find('#shopname').value
+      ).to eq('')
+      expect(
+        find('#shop_shop_category_id').value
+      ).to eq("1")
+      expect(
+        find('#shopdescription').value
+      ).to eq('')
+      expect(
+        find('#shopaddress').value
+      ).to eq('')
+      # 店舗情報を編集する
+      fill_in 'shopname', with: @shop.shop_name
+      select '居酒屋', from: 'shop_shop_category_id'
+      fill_in 'shopdescription', with: @shop.shop_description
+      fill_in 'shopaddress', with: @shop.shop_address
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Shop.count }.by(0)
+      # 詳細画面に遷移したことを確認する
+      expect(current_path).to eq(user_path(@user.id))
+      # 詳細ページで更新したユーザー情報が表示されていることを確認
+      expect(page).to have_content(@shop.shop_name)
+      expect(page).to have_content('居酒屋')
+      expect(page).to have_content(@shop.shop_description)
+      expect(page).to have_content(@shop.shop_address)
+      # 再び店舗情報編集画面へ移動する
+      find('input[name="shop_edit_btn"]').click
+      # 店舗情報フォームに既存の店舗情報が記入されていることを確認する
+      expect(
+        find('#shopname').value
+      ).to eq(@shop.shop_name)
+      expect(
+        find('#shop_shop_category_id').value
+      ).to eq("2")
+      expect(
+        find('#shopdescription').value
+      ).to eq(@shop.shop_description)
+      expect(
+        find('#shopaddress').value
+      ).to eq(@shop.shop_address)
+    end
+  end
+  context '店舗情報の編集ができないとき' do
+    it 'ログインしていないと店舗編集ページへ移動できない' do
+      # トップ画面へ移動する
+      visit root_path
+      # ユーザー変種画面へ移動しようとする
+      visit edit_shop_path(@user.id)
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -137,21 +137,187 @@ RSpec.describe "詳細ページ", type: :system do
       expect(page).to have_content("お店の場所を確認してみましょう！↓　↓　↓")
     end
   end
-  context 'ログインができないとき' do
-    it '保存されているユーザーの情報と合致しないとログインができない' do
-      # # トップページに移動する
-      # visit root_path
-      # # トップページにログインページへ遷移するボタンがあることを確認する
-      # expect(page).to have_content('ログイン')
-      # # ログインページへ遷移する
-      # visit new_user_session_path
-      # # ユーザー情報を入力する
-      # fill_in 'email', with: ''
-      # fill_in 'password', with: ''
-      # # ログインボタンを押す
-      # find('input[name="commit"]').click
-      # # ログインページへ戻されることを確認する
-      # expect(current_path).to eq(new_user_session_path)
+  context '詳細ページを確認できないとき' do
+    it 'ログインしていないと詳細画面へ移動できない' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      visit user_path(@user.id)
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
+
+RSpec.describe "編集", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ユーザー情報の編集ができるとき' do 
+    it 'ログインしたら詳細ページを編集できる' do
+      # ログインする
+      sign_in(@user)
+      # 詳細ページに移動する
+      visit user_path(@user.id)
+      # ユーザー情報編集画面へ移動する
+      find('input[name="user_edit_btn"]').click
+      # すでに登録済みのユーザー情報がフォームに入っていることを確認する
+      expect(
+        find('#nickname').value
+      ).to eq(@user.nickname)
+      expect(
+        find('#profile').value
+      ).to eq(@user.profile)
+      expect(
+        find('#last-name').value
+      ).to eq(@user.family_name_kanji)
+      expect(
+        find('#first-name').value
+      ).to eq(@user.first_name_kanji)
+      expect(
+        find('#last-name-kana').value
+      ).to eq(@user.family_name_kana)
+      expect(
+        find('#first-name-kana').value
+      ).to eq(@user.first_name_kana)
+      expect(
+        find('#user_birthday_1i').value
+      ).to eq(@user.birthday.to_s.slice(0..3))
+      expect(
+        find('#user_birthday_2i').value
+      ).to eq(@user.birthday.to_s.slice(5..6).to_i.to_s)
+      expect(
+        find('#user_birthday_3i').value
+      ).to eq(@user.birthday.to_s.slice(8..9).to_i.to_s)
+      # ユーザー情報を編集する
+      fill_in 'nickname', with: 'テスト中'
+      fill_in 'profile', with: 'テストをしています'
+      fill_in 'last-name', with: '近藤'
+      fill_in 'first-name', with: '秀明'
+      fill_in 'last-name-kana', with: 'コンドウ'
+      fill_in 'first-name-kana', with: 'ヒデアキ'
+      select 1970, from: 'user_birthday_1i'
+      select 1, from: 'user_birthday_2i'
+      select 3, from: 'user_birthday_3i'
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 詳細画面に遷移したことを確認する
+      expect(current_path).to eq(user_path(@user.id))
+      # 詳細ページで更新したユーザー情報が表示されていることを確認
+      expect(page).to have_content('テスト中')
+      expect(page).to have_content('テストをしています')
+      expect(page).to have_content('近藤秀明')
+      expect(page).to have_content('コンドウヒデアキ')
+      expect(page).to have_content('1970-01-03')
+    end
+  end
+  context 'ユーザー情報の編集ができないとき' do
+    it 'ログインしていないと詳細画面へ移動できない' do
+      # トップ画面へ移動する
+      visit root_path
+      # ユーザー変種画面へ移動しようとする
+      visit edit_user_path(@user)
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+    it '必須項目の値を空にすると変更できない' do
+      # ログインする
+      sign_in(@user)
+      # 詳細ページに移動する
+      visit user_path(@user.id)
+      # ユーザー情報編集画面へ移動する
+      find('input[name="user_edit_btn"]').click
+      # すでに登録済みのユーザー情報がフォームに入っていることを確認する
+      expect(
+        find('#nickname').value
+      ).to eq(@user.nickname)
+      expect(
+        find('#profile').value
+      ).to eq(@user.profile)
+      expect(
+        find('#last-name').value
+      ).to eq(@user.family_name_kanji)
+      expect(
+        find('#first-name').value
+      ).to eq(@user.first_name_kanji)
+      expect(
+        find('#last-name-kana').value
+      ).to eq(@user.family_name_kana)
+      expect(
+        find('#first-name-kana').value
+      ).to eq(@user.first_name_kana)
+      expect(
+        find('#user_birthday_1i').value
+      ).to eq(@user.birthday.to_s.slice(0..3))
+      expect(
+        find('#user_birthday_2i').value
+      ).to eq(@user.birthday.to_s.slice(5..6).to_i.to_s)
+      expect(
+        find('#user_birthday_3i').value
+      ).to eq(@user.birthday.to_s.slice(8..9).to_i.to_s)
+      # ユーザー情報を編集する
+      fill_in 'nickname', with: ''
+      fill_in 'profile', with: ''
+      fill_in 'last-name', with: ''
+      fill_in 'first-name', with: ''
+      fill_in 'last-name-kana', with: ''
+      fill_in 'first-name-kana', with: ''
+      select '--', from: 'user_birthday_1i'
+      select '--', from: 'user_birthday_2i'
+      select '--', from: 'user_birthday_3i'
+      # 変更ボタンをクリック
+        find('input[name="commit"]').click
+      # 編集ページへ戻ったことを確認する
+      expect(current_path).to eq("/users/#{@user.id}")
+    end
+  end
+end
+
+RSpec.describe "検索", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @user2 = FactoryBot.build(:user)
+    @user2.nickname = "シンジ"
+  end
+  context 'ユーザーを検索できるとき' do 
+    it 'ログインしたらユーザーを検索できる' do
+      # トップページへ移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがある
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: @user2.nickname
+      fill_in 'profile', with: @user2.profile
+      fill_in 'email', with: @user2.email
+      fill_in 'password', with: @user2.password
+      fill_in 'password-confirmation', with: @user2.password_confirmation
+      fill_in 'last-name', with: @user2.family_name_kanji
+      fill_in 'first-name', with: @user2.first_name_kanji
+      fill_in 'last-name-kana', with: @user2.family_name_kana
+      fill_in 'first-name-kana', with: @user2.first_name_kana
+      select @user2.birthday.to_s.slice(0..3), from: 'user_birthday_1i'
+      select @user2.birthday.to_s.slice(5..6).to_i, from: 'user_birthday_2i'
+      select @user2.birthday.to_s.slice(8..9).to_i, from: 'user_birthday_3i'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がる
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # 正しい情報を入力すればユーザー新規登録ができてトップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログアウトする
+      find(".post-j").click
+      # ログインする
+      sign_in(@user)
+      # 検索したいユーザーのニックネームを検索バーに入力する
+      fill_in 'keyword', with: @user2.nickname
+      # ヘッダーの検索ボタンをクリックする
+      find('input[name="commit"]').click
+      # 検索したユーザー名が表示されていることを確認する
+      expect(page).to have_content(@user2.nickname)
     end
   end
 end
